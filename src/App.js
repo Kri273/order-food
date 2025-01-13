bf6ca4eea5b6c292870e7152404b6cdffc0fd17e
Kri273 <kristelle.tasane@voco.ee>
2025-01-13 13:26:43 +0200

    Ylesanne 1 ja 2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import Meals from "./components/Meals"
+import React from "react";
+
+const App = () => {
+ return (
+    <>
+    <Header />
+     <Meals />
+    </>
+  );
+}
+
+export default App;
